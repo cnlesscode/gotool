b51ae8eaf62ef13ae92b7da49bf690b26051b01f
haijunliu <5213606@qq.com>
2022-12-20 12:38:26 +0800

    v1.2.8

---
+++ gintool/upload.go
@@ -0,0 +1,99 @@
+package gintool
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/cnlesscode/gotool/gfs"
+	"github.com/cnlesscode/gotool/gmd5"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type Upload struct {
+	FileName         string
+	MaxSize          int64
+	AllowExeNames    string
+	AllowTypes       string
+	TargetDir        string
+	DirNamingRule    string
+	FileNamingRule   string
+	UploadedFilePath string
+}
+
+func (m *Upload) Run(ctx *gin.Context) (error, string) {
+	// 检查文件名及文件有效性
+	if m.FileName == "" {
+		return errors.New("请设置上传文件名"), ""
+	}
+	file, err := ctx.FormFile(m.FileName)
+	if err != nil {
+		return errors.New("上传文件数据错误"), ""
+	}
+	// 检查文件大小
+	if m.MaxSize < 1 {
+		m.MaxSize = 10
+	}
+	if file.Size > m.MaxSize*1048576 {
+		return errors.New("上传文件过大"), ""
+	}
+	// 检查文件类型
+	if m.AllowTypes == "" {
+		return errors.New("请设置允许的上传类型"), ""
+	}
+	fileTypes := file.Header["Content-Type"]
+	if len(fileTypes) < 1 {
+		return errors.New("上传文件类型识别错误"), ""
+	}
+	fileType := fileTypes[0]
+	if !strings.Contains(m.AllowTypes, fileType) {
+		return errors.New("上传文件类型错误"), ""
+	}
+	// 检查扩展名
+	if m.AllowExeNames == "" {
+		return errors.New("请设置允许的扩展名"), ""
+	}
+	extendName := strings.ToLower(gfs.GetExtension(file.Filename))
+	Index := strings.Index(m.AllowExeNames, extendName)
+	if Index == -1 {
+		return errors.New("上传文件扩展名错误"), ""
+	}
+	// 检查文件夹是否存在，不存在则创建
+	if m.TargetDir == "" {
+		return errors.New("请设置上传目录"), ""
+	}
+	// 根据文件夹命名规则命名文件夹
+	uploadTargetDir := ""
+	if m.DirNamingRule == "" {
+		uploadTargetDir = m.TargetDir + "/"
+	} else if m.DirNamingRule == "year" {
+		uploadTargetDir = "./static/images/" + time.Now().Format("2006") + "/"
+	} else if m.DirNamingRule == "month" {
+		uploadTargetDir = "./static/images/" + time.Now().Format("2006-01") + "/"
+	} else if m.DirNamingRule == "day" {
+		uploadTargetDir = "./static/images/" + time.Now().Format("2006-01-02") + "/"
+	} else {
+		uploadTargetDir = "./static/images/" + time.Now().Format("2006-01") + "/"
+	}
+	if !gfs.DirExists(uploadTargetDir) {
+		err = gfs.MakeDir(uploadTargetDir)
+		if err != nil {
+			return errors.New("目标文件夹创建失败"), ""
+		}
+	}
+	// 根据文件命名规则命名文件
+	uploadTargetFile := ""
+	if m.FileNamingRule == "" {
+		uploadTargetFile = file.Filename
+	} else if m.FileNamingRule == "random" {
+		uploadTargetFile = gmd5.Md5(uuid.New().String()) + "." + extendName
+	}
+	// 上传文件至指定的完整文件路径
+	err = ctx.SaveUploadedFile(file, uploadTargetDir+uploadTargetFile)
+	if err == nil {
+		return nil, uploadTargetDir + uploadTargetFile
+	} else {
+		return err, ""
+	}
+}
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+
 	"github.com/cnlesscode/gotool/gintool"
 	"github.com/gin-gonic/gin"
 )
@@ -9,9 +12,37 @@ func main() {
 
 	r := gin.Default()
 
-	// 测试路由
-	r.GET("/:name/:age", func(ctx *gin.Context) {
-		gintool.Download(ctx, "README.md", "newname", false)
+	// 处理上传
+	r.POST("/upload", func(ctx *gin.Context) {
+		uploader := &gintool.Upload{}
+		// 文件域名称
+		uploader.FileName = "file"
+		// 文件大小限制, 单位 M
+		uploader.MaxSize = 10
+		// 允许的类型
+		uploader.AllowTypes = "image/jpg,image/jpeg,image/png,image/gif"
+		// 允许的扩展名
+		uploader.AllowExeNames = "jpg,gif,png"
+		// 目标文件夹
+		uploader.TargetDir = "./static/images/"
+		// 文件夹命名规则
+		uploader.DirNamingRule = "month"
+		// 文件命名规则
+		uploader.FileNamingRule = "random"
+
+		err, filePath := uploader.Run(ctx)
+		if err == nil {
+			ctx.JSON(200, gin.H{"filePath": filePath})
+		} else {
+			fmt.Printf("err: %v\n", err)
+			ctx.JSON(200, gin.H{"error": err.Error()})
+		}
+	})
+
+	// 展示上传视图
+	r.GET("/", func(ctx *gin.Context) {
+		t, _ := template.ParseFiles("./templates/upload.html")
+		t.Execute(ctx.Writer, gin.H{})
 	})
 
 	// 监听指定端口
