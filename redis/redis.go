4733814fa8e9d3fd249061e4a35607b939e8f546
GraceCode <5213606@qq.com>
2023-10-24 11:14:19 +0800

    v1.7.2

--- redis/redis.go
+++ redis/redis.go
@@ -67,7 +67,7 @@ func (m *Redis) DoSet(args ...any) error {
 }
 
 // 获取变量 Do 模式
-func (m *Redis) DoGet(args ...any) ([]string, error) {
+func (m *Redis) DoGet(args ...any) ([]any, error) {
 	if m.Client == nil {
 		m.InitRedisClient()
 	}
@@ -75,7 +75,7 @@ func (m *Redis) DoGet(args ...any) ([]string, error) {
 		context.Background(),
 		args...,
 	)
-	return cmd.StringSlice()
+	return cmd.Slice()
 }
 
 func (m *Redis) Close() error {
