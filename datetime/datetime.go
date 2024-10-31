0e8720d27598710a390dfe1b4bbca850154e39de
mogoword <haijun_liu@mogotask.com>
2024-10-31 10:01:40 +0800

    v1.9.5.3

--- datetime/datetime.go
+++ datetime/datetime.go
@@ -64,12 +64,15 @@ func (dt *DateTime) Now() {
 
 // 切换时间
 func (dt *DateTime) Swicth(tp string, val int) *DateTime {
+	if dt.IsLeap() {
+		daysOfMonth[1] = 29
+	}
 	dtNew := New()
 	dtNew.InitFromTimeStamp(dt.TimeStamp)
 	if tp == "year" {
-		dtNew.Time = dt.Time.AddDate(val, 0, 0)
+		dtNew.Time = AddDate(dt.Time, val, 0)
 	} else if tp == "month" {
-		dtNew.Time = dt.Time.AddDate(0, val, 0)
+		dtNew.Time = AddDate(dt.Time, 0, val)
 	} else if tp == "day" {
 		dtNew.Time = dt.Time.AddDate(0, 0, val)
 	} else if tp == "hour" {
@@ -86,4 +89,45 @@ func (dt *DateTime) Swicth(tp string, val int) *DateTime {
 // 获取某月天数
 func (st *DateTime) CountDaysOfAMonth() int {
 	return time.Date(st.Year, st.Time.Month()+1, 0, 0, 0, 0, 0, time.UTC).Day()
+}
+
+// 闰年
+func (dt *DateTime) IsLeap() bool {
+	return dt.Year%4 == 0 && (dt.Year%100 != 0 || dt.Year%400 == 0)
+}
+
+var daysOfMonth = [...]int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+
+func AddDate(t time.Time, years, months int) time.Time {
+	month := t.Month()
+	years, months = norm(years, months, 12)
+	targetMonth := (int(month) + months) % 12
+	targetYear := t.Year() + years + (int(month)+months)/12
+	maxDayOfTargetMonth := daysOfMonth[targetMonth-1]
+	if isLeap(targetYear) && targetMonth == 2 {
+		maxDayOfTargetMonth += 1 // 闰年2月多一天
+	}
+	targetDay := t.Day()
+	if targetDay > maxDayOfTargetMonth {
+		targetDay = maxDayOfTargetMonth
+	}
+	return time.Date(targetYear, time.Month(targetMonth), targetDay, t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), t.Location())
+}
+
+func norm(hi, lo, base int) (nhi, nlo int) {
+	if lo < 0 {
+		n := (-lo-1)/base + 1
+		hi -= n
+		lo += n * base
+	}
+	if lo >= base {
+		n := lo / base
+		hi += n
+		lo -= n * base
+	}
+	return hi, lo
+}
+
+func isLeap(year int) bool {
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
--- gotool_test.go
+++ gotool_test.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/cnlesscode/gotool/nlp"
+	"github.com/cnlesscode/gotool/datetime"
 )
 
 // 单元测试
 // 测试命令 : go test -v -run=TestMain
 func TestMain(t *testing.T) {
-	keyWords, keyWordsString := nlp.Cut("我爱我的祖国！")
-	fmt.Printf("keyWords: %v\n", keyWords)
-	fmt.Printf("keyWordsString: %v\n", keyWordsString)
+	dt := datetime.New()
+	// 初始化当前时间
+	dt.Now()
+	Loger.Printf("当前时间 : %s", dt.Result)
+	// 下一月
+	nextMonth := dt.Swicth("month", 1)
+	Loger.Printf("一月后时间 : %s", nextMonth.Result)
+	nextDay := dt.Swicth("day", 1)
+	fmt.Printf("nextDay: %v\n", nextDay)
 }
