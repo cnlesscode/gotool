0a0df44d539ed072e385fe67ad6a08b0ce0b375b
GraceCode <5213606@qq.com>
2026-01-27 11:17:59 +0800

    v0.1.5
    添加 log 工具

--- gotool_test.go
+++ gotool_test.go
@@ -1,16 +1,16 @@
 package gotool
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/cnlesscode/gotool/gintool"
 )
 
 // 单元测试
 
 // 测试命令 : go test -v -run=TestT
 func TestT(t *testing.T) {
-	AllowTypes := "image/*,image/png,image/gif"
-	fmt.Println(gintool.MatchMimeType(AllowTypes, "image/jpeg"))
+	SetLogLevel(3)
+	LogDebug("debug")
+	LogError("error")
+	LogOk("ok")
+	LogFatal("fatal")
 }
---
+++ log.go
@@ -0,0 +1,43 @@
+package gotool
+
+import (
+	"fmt"
+	"log"
+)
+
+var logLevel = 3
+
+/**
+0 : fatal
+1 : error
+2 : ok
+3 : debug
+*/
+
+func SetLogLevel(level int) {
+	logLevel = level
+}
+
+func LogFatal(msg any) {
+	if logLevel >= 0 {
+		log.Fatalln("\033[31m", fmt.Sprint(msg), "\033[0m")
+	}
+}
+
+func LogError(msg any) {
+	if logLevel >= 1 {
+		log.Println("\033[35m", fmt.Sprint(msg), "\033[0m")
+	}
+}
+
+func LogOk(msg any) {
+	if logLevel >= 2 {
+		log.Println("\033[32m", fmt.Sprint(msg), "\033[0m")
+	}
+}
+
+func LogDebug(msg any) {
+	if logLevel >= 3 {
+		log.Println("\033[30m", fmt.Sprint(msg), "\033[0m")
+	}
+}
