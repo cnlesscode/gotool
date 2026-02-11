db87809964e57f1ba6b71fcdf9c95c29ac70d519
GraceCode <5213606@qq.com>
2026-02-11 17:07:42 +0800

    v0.1.7

--- tcpMessage.go
+++ tcpMessage.go
@@ -1,47 +1,50 @@
 package gotool
 
 import (
-	"bytes"
 	"encoding/binary"
+	"errors"
+	"io"
 	"net"
 )
 
+const MaxMessageSize = 10 << 20 // 10MB
+
 // 响应TCP连接二进制消息
 func WriteTCPResponse(conn net.Conn, msg []byte) error {
-	// int32 占4字节
-	var messageLength int32 = int32(len(msg))
-	var response = new(bytes.Buffer)
-	err := binary.Write(response, binary.LittleEndian, messageLength)
-	if err != nil {
-		return err
+	msgLength := uint32(len(msg))
+	if msgLength > MaxMessageSize {
+		return errors.New("message too long")
 	}
-	// 写入内容
-	err = binary.Write(response, binary.LittleEndian, msg)
-	if err != nil {
-		return err
-	}
-	_, err = conn.Write(response.Bytes())
+	buf := make([]byte, 4+msgLength)
+	binary.LittleEndian.PutUint32(buf[:4], msgLength)
+	copy(buf[4:], msg)
+	_, err := conn.Write(buf)
 	return err
 }
 
 // 读取TCP连接二进制消息
 func ReadTCPResponse(conn net.Conn) ([]byte, error) {
-	// 读取客户端发送的消息
-	buf := make([]byte, 4)
-	n, err := conn.Read(buf)
-	if err != nil || n != 4 {
+	// 1. 读取4字节长度头
+	header := make([]byte, 4)
+	if _, err := io.ReadFull(conn, header); err != nil {
 		return nil, err
 	}
-	var contentLength int32
-	err = binary.Read(bytes.NewBuffer(buf), binary.LittleEndian, &contentLength)
-	if err != nil || n != 4 {
-		return nil, err
+
+	// 2. 解析长度（小端序）
+	contentLength := binary.LittleEndian.Uint32(header)
+	if contentLength == 0 {
+		return []byte{}, nil
 	}
-	// 02. 读取消息
+
+	if contentLength > MaxMessageSize {
+		return nil, errors.New("message too long")
+	}
+
+	// 3. 读取完整消息体
 	contentBuf := make([]byte, contentLength)
-	_, err = conn.Read(contentBuf)
-	if err != nil || n != 4 {
+	if _, err := io.ReadFull(conn, contentBuf); err != nil {
 		return nil, err
 	}
+
 	return contentBuf, nil
 }
